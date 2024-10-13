3a0de70c97c813bff5ffa7baaf6667b98d258975
Yousif-Alsheikh <yousif.r.alsheikh1@gmail.com>
2024-10-13 18:03:29 +0300

    task 02 is done

---
+++ Controllers/Projects.cs
@@ -0,0 +1,62 @@
+﻿using Aon_Freelance.models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aon_Freelance.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class Projects : ControllerBase
+    {
+        private static List<Project> ProjectsList = new List<Project>();
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+
+            return Ok(ProjectsList);
+        }
+             
+        
+        [HttpPost]
+        public IActionResult Create([FromBody] Project project)
+        {
+            ProjectsList.Add(project);
+
+            return CreatedAtAction("Create", new { Id = project.Id }, ProjectsList);
+        }
+
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetFreelancer(int id)
+        {
+            Project pr = ProjectsList.FirstOrDefault(pr => pr.Id == id);
+
+            if (pr == null)
+            {
+                return NotFound("The resoucre is not found!");
+            }
+
+            return Ok(pr);
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Project pr = ProjectsList.FirstOrDefault(pr => pr.Id == id);
+            if (pr != null)
+            {
+                ProjectsList.Remove(pr);
+                return Ok("Deleted");
+
+            }
+
+            return NotFound();
+        }
+
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aon_Freelance.models
+{
+   
+    public class Project
+    {
+
+       
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+    }
+}
